ace95349b1d635ea0167078e3dbe11b4cbf513b6
Valentyn Koshmanyuk <goldenarm31.03@gmail.com>
2025-04-01 21:47:10 +0200

    add title

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navigation from '../Navigation/Navigation';
 import Footer from '../Footer/Footer';
+import styles from './Layout.module.css'
 
 const Layout = ({children}) => {
   return (
     <>
       <header>
+        <div>
+          <p className={styles.name}>Valentyn Koshmanyuk</p>
+          <h1 className={styles.title}>Full-Stack Developer</h1>
+        </div>
          <div>
                   <Navigation/>
         </div>
