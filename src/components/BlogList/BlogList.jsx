6e085d05733bf4aa4751f457d23059cd75f108b5
Valentyn Koshmanyuk <goldenarm31.03@gmail.com>
2025-04-17 12:50:27 +0200

    add blog section

---
+++ src/components/BlogList/BlogList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./BlogList.module.css";
+
+const BlogList = () => {
+  return (
+    <section className={styles.blogGrid}>
+      {[1, 2, 3, 4, 5, 6].map((i) => (
+          <article key={i} className={styles.card}>
+              <img src="" alt="Blog post" className={styles.thumbnail} />
+              <h3 className={styles.cardTitle}>Article Name</h3>
+              <p className={styles.cardDesc}>Short intro about the article...</p>
+              <a href="" className={styles.cardLink}>Read more</a>
+        </article>
+      ))}
+    </section>
+  );
+};
+
+export default BlogList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ProjectList from '../components/ProjectList/ProjectList'
+import BlogList from '../components/BlogList/BlogList'
 
 const Home = () => {
   return (
       <>
-      <ProjectList/>
+      <BlogList/>
     </>
   )
 }
